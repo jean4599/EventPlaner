12e27cc40d62a8c24c85d6623af2f530ec037910
Jean Liu <jean4599@gmail.com>
2020-12-05 17:21:23 +0800

    ! init react typescript webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8000
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
+};
